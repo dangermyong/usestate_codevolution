2f734edb83265365e0c36371a2df71297a5298aa
dangermyong <66460964+dangermyong@users.noreply.github.com>
2020-09-22 08:10:55 +0900

    lec3

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ClassCounter from './ClassCounter';
+import HookCounter from './HookCounter';
+
+function App() {
+  return (
+    <div className='App'>
+      {/*<ClassCounter />*/}
+      <HookCounter />
+    </div>
+  );
+}
+
+export default App;
